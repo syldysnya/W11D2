4ce16b72904328c65d771287ee83e1efcc64aea5
Daniel <danielwu116@gmail.com>
2021-05-04 12:15:08 -0400

    Start guests#show json view

--- jbuilder/config/routes.rb
+++ jbuilder/config/routes.rb
@@ -1,5 +1,22 @@
+# == Route Map
+#
+#          Prefix Verb URI Pattern                           Controller#Action
+#        api_gift GET  /api/gifts/:id(.:format)              api/gifts#show {:format=>:json}
+# api_guest_gifts GET  /api/guests/:guest_id/gifts(.:format) api/gifts#index {:format=>:json}
+#      api_guests GET  /api/guests(.:format)                 api/guests#index {:format=>:json}
+#       api_guest GET  /api/guests/:id(.:format)             api/guests#show {:format=>:json}
+#     api_parties GET  /api/parties(.:format)                api/parties#index {:format=>:json}
+#       api_party GET  /api/parties/:id(.:format)            api/parties#show {:format=>:json}
+
 Rails.application.routes.draw do
   # Your routes here!
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api, defaults: { format: :json } do 
+    resources :gifts, only: [:show]
+    resources :guests, only: [:index, :show] do 
+      resources :gifts, only: [:index]
+    end
+    resources :parties, only: [:index, :show]
+  end
 end
